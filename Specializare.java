3c479deaa8bf11304c48c62eca7e52d20c1ba465
AndreiPatrascan <45433079+AndreiPatrascan@users.noreply.github.com>
2020-03-11 15:32:27 +0200

    ReverseEngineer

---
+++ Specializare.java
@@ -0,0 +1,18 @@
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   Specializare.java
+
+
+public class Specializare
+{
+
+    public Specializare()
+    {
+    }
+
+    private String OptimizareComputationala;
+    public String SistemeDistribuite;
+    private String SecuritateaInformatiei;
+    public String InginerieSoftware;
+}
